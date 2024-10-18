c505e5e4df20d2fff57aeedb680f1005211c6f8a
carloscmp <99369400+carloscmp@users.noreply.github.com>
2024-10-18 14:19:59 -0300

    Feita a ligação do backen com frontend

--- projeto/src/main/java/br/com/markDev/projeto/controller/UsuarioController.java
+++ projeto/src/main/java/br/com/markDev/projeto/controller/UsuarioController.java
@@ -1,9 +1,11 @@
 package br.com.markDev.projeto.controller;
 
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +20,7 @@ import br.com.markDev.projeto.service.UsuarioService;
 
 @RestController
 @RequestMapping(value = "/usuario")
+@CrossOrigin
 public class UsuarioController {
 
 	@Autowired
